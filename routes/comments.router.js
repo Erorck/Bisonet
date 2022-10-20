cef52cb81e25dfecbc1962c30f63cb08bedb0c56
Karim <73983764+Erorck@users.noreply.github.com>
2022-10-19 22:45:04 -0500

    Corrección comments.router y v3 de Endpoints

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -2,7 +2,11 @@ const express = require('express');
 const CommentsService = require('../services/comments.service');
 const service = new CommentsService();
 const validatorHandler = require('../middlewares/validator.handler');
-const {createCommentDto, updateCommentDto, getCommentDto} = require('../DTOs/comments.dto');
+const {
+  createCommentDto,
+  updateCommentDto,
+  getCommentDto,
+} = require('../DTOs/comments.dto');
 
 const router = express.Router();
 
@@ -11,89 +15,94 @@ const router = express.Router();
 //GET ALL COMMENT
 router.get('/', async (req, res, next) => {
   try {
-    const{size} = req.query;
+    const { size } = req.query;
     const filter = req.body;
-    const comments = await service.getAll(size || 10);
+    const comments = await service.getAll(size || 10, filter);
     res.json({
-      'success':true,
-      'message':'Comments found successfully',
-      'Data': comments
-  });
-  } catch (error) {
-    next(error);
-  }
-
-});
-
-
-//CREATE COMMENT
-router.post('/', validatorHandler(createCommentDto, 'body'), async (req, res, next) => {
-  try {
-    const body = req.body;
-    const comment = await service.create(body); //Para updates y creates
-    res.json({
-      'success':true, //Validaciones FrontEnd
-      'message':'Comment created successfully', //Mostrar al usuario
-      'Data': comment //Desplegar información en algún formato
-  });
-  } catch (error) {
-    next(error);
-  }
-
-});
-
-//RUTAS ESPECIFICAS /:id
-
-//GET COMMENT BY ID
-router.get('/:commentId', validatorHandler(getCommentDto, 'params'), async (req, res, next) => {
-  try {
-    const {commentId} = req.params; //Obtener ids
-    const comment = await service.getById(commentId);
-    res.json({
-        'success':true,
-        'message':'Comment found successfully',
-        'Data': comment
+      success: true,
+      message: 'Comments found successfully',
+      Data: comments,
     });
   } catch (error) {
     next(error);
   }
-
 });
+
+//CREATE COMMENT
+router.post(
+  '/',
+  validatorHandler(createCommentDto, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const comment = await service.create(body); //Para updates y creates
+      res.json({
+        success: true, //Validaciones FrontEnd
+        message: 'Comment created successfully', //Mostrar al usuario
+        Data: comment, //Desplegar información en algún formato
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//RUTAS ESPECIFICAS /:id
+
+//GET COMMENT BY ID
+router.get(
+  '/:commentId',
+  validatorHandler(getCommentDto, 'params'),
+  async (req, res, next) => {
+    try {
+      const { commentId } = req.params; //Obtener ids
+      const comment = await service.getById(commentId);
+      res.json({
+        success: true,
+        message: 'Comment found successfully',
+        Data: comment,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 //PARTIALLY UPDATE COMMENT
-router.patch('/:commentId', validatorHandler(getCommentDto, 'params'), validatorHandler(updateCommentDto, 'body'), async (req, res, next) => {
-  try {
-    const {commentId} = req.params; //Obtener ids
-    const body = req.body;
-    const {old, changed} = await service.update(commentId, body);
-    res.json({
-      'success':true,
-      'message':'Comment updated successfully',
-      'Original': old,
-      'Updated': changed
-     });
-  } catch (error) {
-    next(error);
+router.patch(
+  '/:commentId',
+  validatorHandler(getCommentDto, 'params'),
+  validatorHandler(updateCommentDto, 'body'),
+  async (req, res, next) => {
+    try {
+      const { commentId } = req.params; //Obtener ids
+      const body = req.body;
+      const { old, changed } = await service.update(commentId, body);
+      res.json({
+        success: true,
+        message: 'Comment updated successfully',
+        Original: old,
+        Updated: changed,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-
-});
+);
 
 //DELETE COMMENT
 router.delete('/:commentId', async (req, res, next) => {
   try {
-    const {commentId} = req.params; //Obtener ids
+    const { commentId } = req.params; //Obtener ids
     deletedComment = await service.delete(commentId);
     res.json({
-      'success':true,
-      'message':'Comment eliminated successfully',
-      'comment': deletedComment
-  });
+      success: true,
+      message: 'Comment eliminated successfully',
+      comment: deletedComment,
+    });
   } catch (error) {
     next(error);
   }
-
 });
 
 module.exports = router;
-
-
