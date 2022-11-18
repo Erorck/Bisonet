58efcbb3c97192d145f86c85c1bd9162097e77d9
Karim <73983764+Erorck@users.noreply.github.com>
2022-11-17 18:30:41 -0600

    Limpieza de código y Colección Insonmia v5

--- BackEnd/services/photos.service.js
+++ BackEnd/services/photos.service.js
@@ -2,169 +2,92 @@ const faker = require('faker');
 const boom = require('@hapi/boom');
 const PhotosModel = require('../models/photos_post.model');
 
-const NOT_FOUND_COLL_MSG = 'Collection doesn\'t exists';
+const NOT_FOUND_COLL_MSG = "Collection doesn't exists";
 const NO_PHOTOS_REGISTERED_MSG = 'There are no photos registered';
 const PHOTOS_NOT_FOUND_MSG = 'Photos not found: ';
 
-class PhotosService{
-
-  constructor(){
+class PhotosService {
+  constructor() {
     this.photos = [];
     //this.generate();
   }
 
-//BD METHODS-------------------------------------
+  //BD METHODS-------------------------------------
 
-//CREATE DB PHOTOS
-async create(data){
-  const newPhotos = new PhotosModel(data);
-  await newPhotos.save();
-  return data;
-}
+  //CREATE DB PHOTOS
+  async create(data) {
+    const newPhotos = new PhotosModel(data);
+    await newPhotos.save();
+    return data;
+  }
 
-async update(photosId, changes){
-  let photos = await PhotosModel.findOne({
-    _id: photosId
-  });
+  async update(photosId, changes) {
+    let photos = await PhotosModel.findOne({
+      _id: photosId,
+    });
 
-  if(photos == undefined || photos == null)
-    throw new boom.notFound(PHOTOS_NOT_FOUND_MSG + photosId);
+    if (photos == undefined || photos == null)
+      throw new boom.notFound(PHOTOS_NOT_FOUND_MSG + photosId);
 
-  let oldPhotos = {
+    let oldPhotos = {
       post: photos.post,
       content: photos.content,
       Fecha_Publicacion: photos.Fecha_Publicacion,
       isActive: photos.isActive,
-  };
-
-  const {post, content, Fecha_Publicacion, isActive} = changes;
-  photos.post = post || photos.post;
-  photos.content = content || photos.content;
-  photos.Fecha_Publicacion = Fecha_Publicacion || photos.Fecha_Publicacion;
-
-  photos.isActive = isActive || photos.isActive;
-  photos.save();
-
-  return {
-    old: oldPhotos,
-    changed: photos
-  }
-}
-
-async delete(photosId){
-
-  let photos = await PhotosModel.findOne({
-    _id: photosId
-  });
-
-  const {deletedCount} = await PhotosModel.deleteOne({
-    _id: photosId
-  })
-
-  if(deletedCount <= 0)
-    throw new boom.notFound(NOT_FOUND_COLL_MSG + photosId);
-
-  return photos;
-
-}
-
-async getAll(limit, filter){
-  let photos = await PhotosModel.find(filter);
-
-  if(!photos)
-    throw boom.notFound(NOT_FOUND_COLL_MSG);
-  else if(photos.length <= 0)
-    throw boom.notFound(NO_PHOTOS_REGISTERED_MSG);
-
-    photos = limit ? photos.filter((item, index) => item && index < limit) : photos;
-
-  return photos;
-}
-
-async getById(photosId){
-  let photos = await PhotosModel.findOne({
-    _id: photosId
-  });
-
-  if(photos == undefined || photos == null)
-    throw new boom.notFound(PHOTOS_NOT_FOUND_MSG + photosId);
-
-  return photos;
-}
-
-
-//FAKER METHODS-----------------------------------  
-  Fakergenerate(){
-    const limit = 10;
-    for (let index = 0; index <limit; index++) {
-     this.photos.push({
-        photoId : faker.datatype.uuid(),
-        foto : faker.image.image(),
-        upload_date : faker.date.past(),
-        id_post : faker.datatype.uuid(),
-        active : faker.datatype.boolean()
-     });
-
-    }
-  }
-
-  Fakercreate(data){
-    const newphotos = {
-      photoId: faker.datatype.uuid(),
-      ...data //MEZCLAR EL ID CON TODO LO DE DATA
-    }
-    this.photos.push(newphotos);
-    return newphotos;
-  }
-
-  Fakerupdate(id, changes){
-    //const nId = parseInt(id);
-    const index = this.photos.findIndex((item) => item.photoId === id);
-    if(index === -1)
-      throw new boom.notFound('Photos not found: ' + id);
-
-    var currentphotos = this.photos[index];
-    this.photos[index] = {
-      ...currentphotos,
-      ...changes,
     };
+
+    const { post, content, Fecha_Publicacion, isActive } = changes;
+    photos.post = post || photos.post;
+    photos.content = content || photos.content;
+    photos.Fecha_Publicacion = Fecha_Publicacion || photos.Fecha_Publicacion;
+
+    photos.isActive = isActive || photos.isActive;
+    photos.save();
+
     return {
-      old: currentphotos,
-      changed: this.photos[index]
-    }
+      old: oldPhotos,
+      changed: photos,
+    };
   }
 
-  Fakerdelete(id){
-    //const nId = parseInt(id);
-    const index = this.photos.findIndex((item) => item.photoId === id);
-    if(index === -1)
-      throw new boom.notFound('photos not found: ' + id);
+  async delete(photosId) {
+    let photos = await PhotosModel.findOne({
+      _id: photosId,
+    });
 
-    var currentphotos = this.photos[index];
-    this.photos.splice(index, 1);
+    const { deletedCount } = await PhotosModel.deleteOne({
+      _id: photosId,
+    });
 
-    return currentphotos;
-
-  }
-
-  FakergetAll(size){
-    const photos = this.photos.filter((item, index) => item && index < size);
-    if(!photos)
-      throw boom.notFound('Collection doesn´t exists');
-    else if(photos.length <= 0)
-      throw boom.notFound('There are no photos registered');
+    if (deletedCount <= 0)
+      throw new boom.notFound(NOT_FOUND_COLL_MSG + photosId);
 
     return photos;
   }
 
-  FakergetById(id){
-    //const nId = parseInt(id);
-    const photos = this.photos.find((item) => item && item.photoId === id);
-    if(!photos)
-      throw new boom.notFound('photos not found: ' + id);
+  async getAll(limit, filter) {
+    let photos = await PhotosModel.find(filter);
+
+    if (!photos) throw boom.notFound(NOT_FOUND_COLL_MSG);
+    else if (photos.length <= 0) throw boom.notFound(NO_PHOTOS_REGISTERED_MSG);
+
+    photos = limit
+      ? photos.filter((item, index) => item && index < limit)
+      : photos;
+
     return photos;
   }
 
+  async getById(photosId) {
+    let photos = await PhotosModel.findOne({
+      _id: photosId,
+    });
+
+    if (photos == undefined || photos == null)
+      throw new boom.notFound(PHOTOS_NOT_FOUND_MSG + photosId);
+
+    return photos;
+  }
 }
 
 module.exports = PhotosService;
--- BackEnd/services/post.service.js
+++ BackEnd/services/post.service.js
@@ -2,34 +2,33 @@ const faker = require('faker');
 const boom = require('@hapi/boom');
 const PostModel = require('../models/posts.model');
 
-const NOT_FOUND_COLL_MSG = 'Collection doesn\'t exists';
+const NOT_FOUND_COLL_MSG = "Collection doesn't exists";
 const NO_POST_REGISTERED_MSG = 'There are no post registered';
 const POST_NOT_FOUND_MSG = 'Post not found: ';
 
-class postService{
-
-  constructor(){
+class postService {
+  constructor() {
     this.post = [];
     //this.generate();
   }
 
-//DB METHODS-----------------------------------------
+  //DB METHODS-----------------------------------------
 
-async create(data){
-  const newPost = new PostModel(data);
-  await newPost.save();
-  return data;
-}
+  async create(data) {
+    const newPost = new PostModel(data);
+    await newPost.save();
+    return data;
+  }
 
-async update(PostId, changes){
-  let Post = await PostModel.findOne({
-    _id: PostId
-  });
+  async update(PostId, changes) {
+    let Post = await PostModel.findOne({
+      _id: PostId,
+    });
 
-  if(Post == undefined || Post == null)
-    throw new boom.notFound(POST_NOT_FOUND_MSG + PostId);
+    if (Post == undefined || Post == null)
+      throw new boom.notFound(POST_NOT_FOUND_MSG + PostId);
 
-  let oldPost = {
+    let oldPost = {
       Autor: Post.Autor,
       Seccion: Post.Seccion,
       Type: Post.Type,
@@ -40,146 +39,74 @@ async update(PostId, changes){
       Fecha_Modificacion: Post.Fecha_Modificacion,
       Likes: Post.Likes,
       isActive: Post.isActive,
-  };
+    };
 
-  const {Autor, Seccion, Type, Group, Title, Content, Fecha_Publicacion, Fecha_Modificacion, Likes, isActive} = changes;
-  Post.Autor = Autor || Post.Autor;
-  Post.Seccion = Seccion || Post.Seccion;
-  Post.Type = Type || Post.Type;
-  Post.Group = Group || Post.Group;
-  Post.Title = Title || Post.Title;
-  Post.Content = Content || Post.Content;
-  Post.Fecha_Publicacion = Fecha_Publicacion || Post.Fecha_Publicacion;
-  Post.Fecha_Modificacion = Fecha_Modificacion || Post.Fecha_Modificacion;
-  Post.Likes = Likes || Post.Likes;
+    const {
+      Autor,
+      Seccion,
+      Type,
+      Group,
+      Title,
+      Content,
+      Fecha_Publicacion,
+      Fecha_Modificacion,
+      Likes,
+      isActive,
+    } = changes;
+    Post.Autor = Autor || Post.Autor;
+    Post.Seccion = Seccion || Post.Seccion;
+    Post.Type = Type || Post.Type;
+    Post.Group = Group || Post.Group;
+    Post.Title = Title || Post.Title;
+    Post.Content = Content || Post.Content;
+    Post.Fecha_Publicacion = Fecha_Publicacion || Post.Fecha_Publicacion;
+    Post.Fecha_Modificacion = Fecha_Modificacion || Post.Fecha_Modificacion;
+    Post.Likes = Likes || Post.Likes;
 
-  Post.isActive = isActive || Post.isActive;
-  Post.save();
+    Post.isActive = isActive || Post.isActive;
+    Post.save();
 
-  return {
-    old: oldPost,
-    changed: Post
+    return {
+      old: oldPost,
+      changed: Post,
+    };
   }
-}
 
-async delete(postId){
+  async delete(postId) {
+    let Post = await PostModel.findOne({
+      _id: postId,
+    });
 
-  let Post = await PostModel.findOne({
-    _id: postId
-  });
+    const { deletedCount } = await PostModel.deleteOne({
+      _id: postId,
+    });
 
-  const {deletedCount} = await PostModel.deleteOne({
-    _id: postId
-  })
+    if (deletedCount <= 0) throw new boom.notFound(NOT_FOUND_COLL_MSG + postId);
 
-  if(deletedCount <= 0)
-    throw new boom.notFound(NOT_FOUND_COLL_MSG + postId);
+    return Post;
+  }
 
-  return Post;
+  async getAll(limit, filter) {
+    let Post = await PostModel.find(filter);
 
-}
-
-async getAll(limit, filter){
-  let Post = await PostModel.find(filter);
-
-  if(!Post)
-    throw boom.notFound(NOT_FOUND_COLL_MSG);
-  else if(Post.length <= 0)
-    throw boom.notFound(NO_POST_REGISTERED_MSG);
+    if (!Post) throw boom.notFound(NOT_FOUND_COLL_MSG);
+    else if (Post.length <= 0) throw boom.notFound(NO_POST_REGISTERED_MSG);
 
     Post = limit ? Post.filter((item, index) => item && index < limit) : Post;
 
-  return Post;
-}
-
-async getById(postId){
-  let Post = await PostModel.findOne({
-    _id: postId
-  });
-
-  if(Post == undefined || Post == null)
-    throw new boom.notFound(POST_NOT_FOUND_MSG + postId);
-
-  return Post;
-}
-
-
-//FAKER METHODS----------------------------------------
-Fakergenerate(){
-    const limit = 10;
-    for (let index = 0; index <limit; index++) {
-     this.post.push({
-        postId : faker.datatype.uuid(),
-        autor : faker.datatype.uuid(),
-        section : faker.datatype.uuid(),
-        post_type: faker.name.jobArea(),
-        title : faker.lorem.sentence(),
-        content : faker.lorem.text(),
-        publication_date : faker.date.past(),
-        modification_date : faker.date.past(),
-        active : faker.datatype.boolean()
-     });
-
-    }
+    return Post;
   }
 
-  Fakercreate(data){
-    const newpost = {
-      postId: faker.datatype.uuid(),
-      ...data //MEZCLAR EL ID CON TODO LO DE DATA
-    }
-    this.post.push(newpost);
-    return newpost;
+  async getById(postId) {
+    let Post = await PostModel.findOne({
+      _id: postId,
+    });
+
+    if (Post == undefined || Post == null)
+      throw new boom.notFound(POST_NOT_FOUND_MSG + postId);
+
+    return Post;
   }
-
-  Fakerupdate(id, changes){
-    //const nId = parseInt(id);
-    const index = this.post.findIndex((item) => item.postId === id);
-    if(index === -1)
-      throw new boom.notFound('post not found: ' + id);
-
-    var currentpost = this.post[index];
-    this.post[index] = {
-      ...currentpost,
-      ...changes,
-    };
-    return {
-      old: currentpost,
-      changed: this.post[index]
-    }
-  }
-
-  Fakerdelete(id){
-    //const nId = parseInt(id);
-    const index = this.post.findIndex((item) => item.postId === id);
-    if(index === -1)
-      throw new boom.notFound('post not found: ' + id);
-
-    var currentpost = this.post[index];
-    this.post.splice(index, 1);
-
-    return currentpost;
-
-  }
-
-  FakergetAll(size){
-    const post = this.post.filter((item, index) => item && index < size);
-    if(!post)
-      throw boom.notFound('post doesn´t exists');
-    else if(post.length <= 0)
-      throw boom.notFound('There are no post registered');
-
-    return post;
-  }
-
-  FakergetById(id){
-    //const nId = parseInt(id);
-    const post = this.post.find((item) => item && item.postId === id);
-    if(!post)
-      throw new boom.notFound('post not found: ' + id);
-    return post;
-  }
-
 }
 
 module.exports = postService;
